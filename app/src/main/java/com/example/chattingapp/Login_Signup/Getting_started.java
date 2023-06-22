4ea43424836d10c666649c97a198d406d15071a3
Matruprasad <matruprasadsahoo135@gmaill.com>
2023-06-22 10:56:40 +0530

    Merge pull request #1 from Matru720/master
    signup page design

--- app/src/main/java/com/example/chattingapp/Login_Signup/Getting_started.java
+++ app/src/main/java/com/example/chattingapp/Login_Signup/Getting_started.java
@@ -5,18 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.chattingapp.R;
 import com.example.chattingapp.signup_login.Signup_page;
 
 public class Getting_started extends AppCompatActivity {
 
-    Button btn_getStarted;
+    TextView getstarted;
+
+    LottieAnimationView lottie;
+
+   Button btn_getStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getting_started_screen);
+
+        getstarted = findViewById(R.id.getstarted);
+        lottie = findViewById(R.id.lottie);
+
+        lottie.playAnimation();
 
         btn_getStarted = findViewById(R.id.btn_get_started);
 
--- app/src/main/java/com/example/chattingapp/Startups/Splash_screen.java
+++ app/src/main/java/com/example/chattingapp/Startups/Splash_screen.java
@@ -5,16 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.chattingapp.Login_Signup.Getting_started;
 import com.example.chattingapp.R;
 
 public class Splash_screen extends AppCompatActivity {
 
+    TextView appearance;
+    LottieAnimationView lottie;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        appearance = findViewById(R.id.appearance);
+        lottie = findViewById(R.id.lottie);
+
+
+        appearance.animate().translationY(-400).setDuration(700).setStartDelay(50);
+        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+        lottie.playAnimation();
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
